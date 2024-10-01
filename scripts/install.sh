6889263a461fa2080cb0307ea2575d8362307faa
ra0x3 <spam.rashad@protonmail.com>
2024-10-01 22:13:10 +0000

    fix: update install for linux

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
+OS=$(uname)
+
 install_mac() {
     echo "Detected macOS"
 
     export HOMEBREW_NO_AUTO_UPDATE=1
 
-    if ! command -v brew &> /dev/null; then
+    if ! command -v brew > /dev/null 2>&1; then
         echo "Homebrew not found. Installing Homebrew..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
 
     echo "Checking for Rust installation..."
-    if command -v rustc &> /dev/null; then
+    if command -v rustc > /dev/null 2>&1; then
         echo "Rust is installed. Setting to version 1.78..."
         rustup install 1.78.0
         rustup default 1.78.0
@@ -36,28 +38,29 @@ install_linux() {
     sudo apt-get install -y libssl-dev pkg-config git curl
 
     echo "Checking for Rust installation..."
-    if command -v rustc &> /dev/null; then
+    if command -v rustc > /dev/null 2>&1; then
         echo "Rust is installed. Setting to version 1.78..."
         rustup install 1.78.0
         rustup default 1.78.0
     else
         echo "Rust not found. Installing Rust 1.78..."
         curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain 1.78.0 -y
-        source $HOME/.cargo/env
+        . $HOME/.cargo/env
     fi
 
     echo "Installing WireGuard"
-    sudo apt-get install wireguard
+    sudo apt-get install -y wireguard
 
 }
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if [ "$OS" = "Darwin" ]; then
     install_mac
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+elif [ "$OS" = "Linux" ]; then
     install_linux
 else
-    echo "Unsupported platform: $OSTYPE"
+    echo "Unsupported platform: $OS"
     exit 1
 fi
 
 echo "All dependencies installed successfully!"
+
